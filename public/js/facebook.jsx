d8e012ae508bbf9b35289da86204d1233cd7b652
Linda Nichols <lynnaloo@gmail.com>
2014-09-12 18:58:40 +0000

    Changed react include to version with plugins

--- public/js/facebook.jsx
+++ public/js/facebook.jsx
@@ -1,6 +1,18 @@
 /** @jsx React.DOM */
 
 var Facebook = React.createClass({
+  getInitialState: function() {
+    return {secondsElapsed: 0};
+  },
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
   render: function() {
     return (
       <div>
@@ -11,6 +23,7 @@ var Facebook = React.createClass({
         <p>
           Party on!
         </p>
+        <div>Seconds Elapsed: {this.state.secondsElapsed}</div>
       </div>
     );
   }
