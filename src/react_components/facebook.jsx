fee9f975b05c57842e5569666df34031c9080f6e
Linda Nichols <lynnaloo@gmail.com>
2015-01-13 05:53:04 +0000

    Added some css and logo styling

--- src/react_components/facebook.jsx
+++ src/react_components/facebook.jsx
@@ -15,12 +15,18 @@
     render: function() {
       return (
         <div className="container">
-          <h1 className="page-header">Welcome to the Mullet Stack.</h1>
-          <p className="lead">Facebook in the front. Walmart in the back.</p>
-
-          <div className="footer">
-            <p className="text-muted">Created by <a href="http://github.com/lynnaloo/">@lynnaloo</a></p>
-          </div>
+          <header>
+            <div className="container">
+              <div className="row">
+                <div className="col-lg-12">
+                  <img className="img-responsive" src="/images/mullet_600.png" width="200" alt=""/>
+                  <p className="title">Welcome to the Mullet Stack.</p>
+                  <p className="subtitle">Facebook in the front. Walmart in the back.</p>
+                  <p className="subtitle">Created by <a href="http://github.com/lynnaloo/">@lynnaloo</a></p>
+                </div>
+              </div>
+            </div>
+          </header>
         </div>
       );
     }
