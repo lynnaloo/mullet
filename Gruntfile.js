146aafb767afc88fa1b6457fdfff52b142fd2151
Hannu-Pekka Heinajarvi <hannupekka@gmail.com>
2015-10-02 22:58:57 +0300

    Remove Bower and use npm instead.

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,11 +25,20 @@
       },
       'eslint': {
         src: ['src/react_components/main.js']
+      },
+      'copy': {
+        main: {
+          files: [
+            {expand: true, flatten: true, src: ['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.*'], dest: 'public/js'},
+            {expand: true, flatten: true, src: ['node_modules/bootstrap/dist/css/bootstrap*.min.css'], dest: 'public/css'},
+          ]
+        }
       }
     });
 
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('gruntify-eslint');
 
     grunt.registerTask('default', ['browserify']);
