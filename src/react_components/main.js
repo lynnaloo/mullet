f13651cec2a2bdbbd852a410a8af2e00a672bd3e
Weyland Joyner <weylandjoyner4@gmail.com>
2016-01-06 11:09:21 -0800

    more modernization

--- src/react_components/main.js
+++ src/react_components/main.js
@@ -1,13 +1,19 @@
-'use strict';
-
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
 import Facebook from './facebook';
 
+export default class Main extends Component {
+  render() {
+    return (
+      <Facebook
+        title="Welcome to the Mullet Stack."
+        subtitle="Facebook in the front. Walmart in the back."
+      />
+    );
+  }
+};
+
 ReactDOM.render(
-  <Facebook
-    title="Welcome to the Mullet Stack."
-    subtitle="Facebook in the front. Walmart in the back."
-  />,
+  <Main />,
   document.getElementById('facebook')
 );
